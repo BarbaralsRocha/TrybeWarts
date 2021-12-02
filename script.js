5b8cd35e941fa659777cfead367754730bf55535
BarbaraRocha <barbaraluizasr@gmail.com>
2021-12-02 19:06:32 -0300

    todos os requisitos obrigatorios

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const buttonHeader = document.getElementById('button-header');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
+const check = document.querySelector('#agreement');
+const formsButton = document.querySelector('#submit-btn');
 
 buttonHeader.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
+  }
+});
+
+formsButton.disabled = true;
+
+check.addEventListener('click', () => {
+  if (check.checked) {
+    formsButton.disabled = false;
   }
 });
