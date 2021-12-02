1455e9a6f295c64da76197d02210faaea620b65a
BarbaraRocha <barbaraluizasr@gmail.com>
2021-12-02 19:18:06 -0300

    correção erros

--- script.js
+++ script.js
@@ -13,13 +13,9 @@ buttonHeader.addEventListener('click', () => {
 });
 
 formsButton.disabled = true;
-
 check.addEventListener('click', () => {
   if (check.checked) {
     formsButton.disabled = false;
   }
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> f6b9fb7b62a5c1c24a630b6a15c58589ae5010fd
+
