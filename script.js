3cd11a356bace83393d67e8fcc7afc2fda86f7cb
BarbaraRocha <barbaraluizasr@gmail.com>
2021-12-02 19:57:53 -0300

    teste erro merge

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const check = document.querySelector('#agreement');
 const formsButton = document.querySelector('#submit-btn');
+const textarea = document.getElementById('textarea');
+const counter = document.getElementById('counter');
 
 buttonHeader.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -19,3 +21,15 @@ check.addEventListener('click', () => {
   }
 });
 
+function countCaracter() {
+  textarea.value = '';
+  const words = textarea.value.split('');
+  let contador = 500;
+  textarea.addEventListener('keyup', () => {
+    for (let i = 0; i < words.length; i += 1) {
+      contador -= 1;
+      counter.innerText = contador;
+    }
+  });
+}
+countCaracter();
