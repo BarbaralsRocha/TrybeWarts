3ab9013c2776c608318093baff3b924d0dc7a429
Sabrina <stx.sabrina@gmail.com>
2021-12-06 14:16:22 -0300

    commit teste

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const check = document.querySelector('#agreement');
 const formsButton = document.querySelector('#submit-btn');
-
+const textarea = document.getElementById('textarea');
+const counter = document.getElementById('counter');
 buttonHeader.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
@@ -11,11 +12,48 @@ buttonHeader.addEventListener('click', () => {
     alert('Email ou senha inválidos.');
   }
 });
-
 formsButton.disabled = true;
 check.addEventListener('click', () => {
   if (check.checked) {
     formsButton.disabled = false;
   }
 });
-
+function countCaracter() {
+  textarea.value = '';
+  const contador = 500;
+  let caracter = 0;
+  textarea.addEventListener('keyup', () => {
+    if (contador >= textarea.value.length) {
+      caracter = contador - textarea.value.length;
+      counter.innerText = `Caracteres: ${caracter}`;
+    }
+  });
+}
+countCaracter();
+const materias = document.querySelectorAll('.subject');
+function checkboxValidation() {
+  let materiasChecked = '';
+  for (let i = 0; i < materias.length; i += 1) {
+    if (materias[i].checked) {
+      materiasChecked += ` ${materias[i].value}`;
+    }
+  }
+  return materiasChecked;
+}
+function validate() {
+  formsButton.addEventListener('click', () => {
+    const name = document.getElementById('input-name').value;
+    const lastname = document.getElementById('input-lastname').value;
+    const emailInput = document.getElementById('input-email').value;
+    const house = document.getElementById('house').value;
+    const family = document.querySelector('input[name="family"]:checked').value; // referencia :https://backefront.com.br/obter-valor-selecionado-radio-button/
+    const avaliacao = document.querySelector('input[name="rate"]:checked').value;
+    const forms = document.getElementById('evaluation-form');
+    const validateForms = document.getElementById('validate-forms');
+    const observacoes = document.getElementById('textarea').value;
+    forms.innerHTML = '';
+    const formulario = `Nome: ${name} ${lastname}\n Email: ${emailInput}\n Casa: ${house}\n Família: ${family}\n Matérias: ${checkboxValidation()}\n Avaliação: ${avaliacao}\n Observações: ${observacoes}\n`;
+    validateForms.innerText = formulario;
+  });
+}
+validate();
